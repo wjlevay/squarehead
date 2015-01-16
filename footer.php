184eda10827c67284f1602292ccf95b7e70bf855
wjlevay <wjlevay@gmail.com>
2015-01-15 19:29:16 -0500

    1st commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+
+				<div id="inner-footer" class="wrap cf">
+
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></p>
+
+				</div>
+
+			</footer>
+
+		</div>
+
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end of site. what a ride! -->
---
+++ front-page.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+					<main id="main" class="m-all t-all d-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+						<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+							<section class="entry-content cf" itemprop="articleBody">
+								<?php
+									// the content (pretty self explanatory huh)
+									the_content();
+								?>
+							</section> <?php // end article section ?>
+
+						</article>
+
+						<?php endwhile; else : ?>
+
+								<article id="post-not-found" class="hentry cf">
+									<header class="article-header">
+										<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+									</header>
+									<section class="entry-content">
+										<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+									</section>
+									<footer class="article-footer">
+											<p><?php _e( 'This is the error message in the front-page.php template.', 'bonestheme' ); ?></p>
+									</footer>
+								</article>
+
+						<?php endif; ?>
+
+					</main>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
